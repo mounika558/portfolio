68d63441eb81935ac4afa14ec5e17a151d5c645f
2004mounika <mounika.2205058@srec.ac.in>
2024-12-20 21:13:23 +0530

    initail commit

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,19 @@
-const name=document.getElementById('name').value;
-const content=document.getElementById('loc').value;
-const email=document.getElementById('email').value;
-console.log(name);
+function details(){
+    const name=document.getElementById('name').value;
+    const content=document.getElementById('loc').value;
+    const email=document.getElementById('email').value;
+
+    if((!name)&&(!email)){
+    alert('please enter all details before submitting');
+    return;
+    }
+    else if(!email||!/^\S+@\S+\.\S+$/.test(email)){
+    alert("Incorrect Email!!");
+    return;
+    }
+    else{
+    alert('details shared successfully');
+    return;
+    }
+
+}
